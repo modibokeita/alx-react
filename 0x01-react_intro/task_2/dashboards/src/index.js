cc933c47e8b7d68f888b672fe89c4208d3579506
modibo keita <93909081keita@gmail.com>
2024-11-01 11:53:31 +0200

    task_2

---
+++ 0x01-react_intro/task_2/dashboards/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import Notifications  from './Notifications';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+
+  </React.StrictMode>
+);
+
+const root2 = ReactDOM.createRoot(document.getElementById('root-notifications'));
+root2.render(
+  <React.StrictMode>
+
+    <Notifications />
+    
+  </React.StrictMode>,
+)
+
