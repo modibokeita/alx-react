75bf9efac385f286a089d85a84e67e8c5c94a7e3
modibo keita <93909081keita@gmail.com>
2024-11-05 15:43:59 +0200

    task_1

---
+++ 0x02-react_props/task_2/dashboards/src/App/App.js
@@ -0,0 +1,24 @@
+// src/App/App.js
+import React from 'react';
+import './App.css';
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Login from '../Login/Login';
+
+function App() {
+  return (
+    <>
+      <Notifications />
+      <div className="App">
+        <Header />
+        <div className="App-body">
+          <Login />
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ 0x02-react_props/task_2/dashboards/src/Notifications/Notifications.test.js
@@ -0,0 +1,23 @@
+// src/Notifications/Notifications.test.js
+import React from 'react';
+import { shallow } from 'enzyme';
+import {Notifications} from './Notifications';
+import NotificationItem from './NotificationItem';
+
+describe('Notifications Component', () => {
+  it('renders without crashing', () => {
+    shallow(<Notifications />);
+  });
+
+  it('renders NotificationItem components', () => {
+    const wrapper = shallow(<Notifications />);
+    expect(wrapper.find(NotificationItem).length).toBe(3);
+  });
+
+  it('renders the first NotificationItem with the correct html', () => {
+    const wrapper = shallow(<Notifications />);
+    const firstItem = wrapper.find(NotificationItem).first();
+    expect(firstItem.prop('type')).toBe('default');
+    expect(firstItem.prop('value')).toBe('New course available');
+  });
+});
