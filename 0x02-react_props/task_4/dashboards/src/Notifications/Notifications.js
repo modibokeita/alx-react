77e45191fa94e8c19e2f002cb49b9e7623fbb43b
modibo keita <93909081keita@gmail.com>
2024-11-05 15:57:40 +0200

    task_4

---
+++ 0x02-react_props/task_4/dashboards/src/Notifications/Notifications.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Notifications.css"
+//import closeIcon from './close-icon.png';
+export function Notifications() {
+  return (
+    <div className="Notifications">
+      <h1>Note</h1>
+    </div>
+  );
+}
