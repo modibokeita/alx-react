2131cd5c98dc3345379c446c8220dda8866a0f34
modibo keita <93909081keita@gmail.com>
2024-11-07 12:43:24 +0200

    course list

--- 0x03-React_component/task_3/dashboards/src/App/App.js
+++ 0x03-React_component/task_3/dashboards/src/App/App.js
@@ -1,24 +1,37 @@
-// src/App/App.js
+// task_3/dashboard/src/App/App.js
 import React from 'react';
 import './App.css';
-import Notifications from '../Notifications/Notifications';
+import { Notifications } from '../Notifications/Notifications';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Login from '../Login/Login';
+import CourseList from '../CourseList/CourseList';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+import BodySection from '../BodySection/BodySection';
 
-function App() {
-  return (
-    <>
-      <Notifications />
-      <div className="App">
-        <Header />
-        <div className="App-body">
-          <Login />
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Notifications />
+        <div className="App">
+          <Header />
+          <div className="App-body">
+            <BodySectionWithMarginBottom title="Course list">
+              <CourseList />
+            </BodySectionWithMarginBottom>
+            <BodySectionWithMarginBottom title="Log in to continue">
+              <Login />
+            </BodySectionWithMarginBottom>
+            <BodySection title="News from the School">
+              <p>Here is some random text to represent news from the school.</p>
+            </BodySection>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
 
 export default App;
