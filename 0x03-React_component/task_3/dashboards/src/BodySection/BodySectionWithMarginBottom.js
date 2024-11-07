a99ce8da0e254a3686103a7e7034f51cc55a17b5
modibo keita <93909081keita@gmail.com>
2024-11-07 12:40:44 +0200

    body component

---
+++ 0x03-React_component/task_3/dashboards/src/BodySection/BodySectionWithMarginBottom.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BodySectionWithMarginBottom from './BodySectionWithMarginBottom';
+
+function Example() {
+  return (
+    <BodySectionWithMarginBottom title="Example Title">
+      <p>Example content inside BodySectionWithMarginBottom</p>
+    </BodySectionWithMarginBottom>
+  );
+}
+
+export default Example;
