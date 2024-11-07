4b5896a3625787e72e3dd3a8193c39f6c484fec2
modibo keita <93909081keita@gmail.com>
2024-11-07 12:17:59 +0200

    test files

--- 0x03-React_component/task_1/dashboards/src/Notifications/Notifications.js
+++ 0x03-React_component/task_1/dashboards/src/Notifications/Notifications.js
@@ -1,10 +1,47 @@
-import React from 'react'
-import "./Notifications.css"
-//import closeIcon from './close-icon.png';
-export function Notifications() {
-  return (
-    <div className="Notifications">
-      <h1>Note</h1>
-    </div>
-  );
+// src/Notifications/Notifications.js
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import "./Notifications.css";
+import "./NotificationItem";
+class Notifications extends Component {
+  constructor(props) {
+    super(props);
+    this.markAsRead = this.markAsRead.bind(this);
+  }
+
+  markAsRead(id) {
+    console.log(`Notification ${id} has been marked as read`);
+  }
+
+  render() {
+    return (
+      <div className="Notifications">
+        <h1>Note</h1>
+        {/* Assuming NotificationItem is a component that takes id and markAsRead as props */}
+        {this.props.notifications.map((notification) => (
+          <NotificationItem
+            key={notification.id}
+            id={notification.id}
+            text={notification.text}
+            markAsRead={this.markAsRead}
+          />
+        ))}
+      </div>
+    );
+  }
 }
+
+Notifications.propTypes = {
+  notifications: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+Notifications.defaultProps = {
+  notifications: [],
+};
+
+export default Notifications;
