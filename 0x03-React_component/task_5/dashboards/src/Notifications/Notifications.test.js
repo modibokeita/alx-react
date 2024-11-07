907249e91a36268b17d062323150148c2439307f
modibo keita <93909081keita@gmail.com>
2024-11-07 13:01:04 +0200

    test files

--- 0x03-React_component/task_5/dashboards/src/Notifications/Notifications.test.js
+++ 0x03-React_component/task_5/dashboards/src/Notifications/Notifications.test.js
@@ -1,23 +1,46 @@
-// src/Notifications/Notifications.test.js
 import React from 'react';
 import { shallow } from 'enzyme';
-import {Notifications} from './Notifications';
-import NotificationItem from './NotificationItem';
+import Notifications from './Notifications';
 
-describe('Notifications Component', () => {
-  it('renders without crashing', () => {
-    shallow(<Notifications />);
+describe('Notifications component', () => {
+  it('should not re-render when updating with the same list', () => {
+    // Initial list of notifications
+    const listNotifications = ['Notification 1', 'Notification 2'];
+
+    // Spy on the render method to track rerenders
+    const spy = jest.spyOn(Notifications.prototype, 'render');
+
+    // Shallow render the component
+    const wrapper = shallow(<Notifications listNotifications={listNotifications} />);
+
+    // Set the same props again (same list)
+    wrapper.setProps({ listNotifications });
+
+    // Check that the render method has not been called again
+    expect(spy).toHaveBeenCalledTimes(1);
+
+    // Restore the spy
+    spy.mockRestore();
   });
 
-  it('renders NotificationItem components', () => {
-    const wrapper = shallow(<Notifications />);
-    expect(wrapper.find(NotificationItem).length).toBe(3);
-  });
+  it('should re-render when updating with a longer list', () => {
+    // Initial list of notifications
+    const listNotifications = ['Notification 1', 'Notification 2'];
 
-  it('renders the first NotificationItem with the correct html', () => {
-    const wrapper = shallow(<Notifications />);
-    const firstItem = wrapper.find(NotificationItem).first();
-    expect(firstItem.prop('type')).toBe('default');
-    expect(firstItem.prop('value')).toBe('New course available');
+    // Spy on the render method to track rerenders
+    const spy = jest.spyOn(Notifications.prototype, 'render');
+
+    // Shallow render the component
+    const wrapper = shallow(<Notifications listNotifications={listNotifications} />);
+
+    // Set new props with a longer list
+    const newListNotifications = ['Notification 1', 'Notification 2', 'Notification 3'];
+    wrapper.setProps({ listNotifications: newListNotifications });
+
+    // Check that the render method has been called again because the list is longer
+    expect(spy).toHaveBeenCalledTimes(2);
+
+    // Restore the spy
+    spy.mockRestore();
   });
 });
