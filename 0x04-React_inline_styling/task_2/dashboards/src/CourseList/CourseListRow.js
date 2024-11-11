a0f0c45e1823663c2f298afcb5a2470305a8cc2e
modibo keita <93909081keita@gmail.com>
2024-11-11 19:15:33 +0200

    inline styles in courseListRow component

--- 0x04-React_inline_styling/task_2/dashboards/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_2/dashboards/src/CourseList/CourseListRow.js
@@ -4,25 +4,22 @@ import PropTypes from 'prop-types';
 import { StyleSheet, css } from 'aphrodite';
 
 const CourseListRow = ({ isHeader, textFirstCell, textSecondCell = null }) => {
-    // Define styles for header and regular rows
-    const headerRowStyle = { backgroundColor: '#deb5b545' };
-    const regularRowStyle = { backgroundColor: '#f5f5f5ab' };
-
+    // Render the row with Aphrodite styles
     return (
-        <tr style={isHeader ? headerRowStyle : regularRowStyle}>
+        <tr className={isHeader ? css(styles.headerRow) : css(styles.regularRow)}>
             {isHeader ? (
                 textSecondCell ? (
                     <>
-                        <th className={css(styles.red)}>{textFirstCell}</th>
-                        <th>{textSecondCell}</th>
+                        <th className={css(styles.headerCell, styles.red)}>{textFirstCell}</th>
+                        <th className={css(styles.headerCell)}>{textSecondCell}</th>
                     </>
                 ) : (
-                    <th colSpan={2}>{textFirstCell}</th>
+                    <th colSpan={2} className={css(styles.headerCell)}>{textFirstCell}</th>
                 )
             ) : (
                 <>
-                    <td className={css(styles.header)}>{textFirstCell}</td>
-                    <td>{textSecondCell}</td>
+                    <td className={css(styles.regularCell)}>{textFirstCell}</td>
+                    <td className={css(styles.regularCell)}>{textSecondCell}</td>
                 </>
             )}
         </tr>
@@ -40,12 +37,25 @@ CourseListRow.defaultProps = {
 };
 
 const styles = StyleSheet.create({
+    // Define styles for rows
+    headerRow: {
+        backgroundColor: '#deb5b545',
+    },
+    regularRow: {
+        backgroundColor: '#f5f5f5ab',
+    },
+    // Define styles for header cells
+    headerCell: {
+        padding: '10px',
+        fontWeight: 'bold',
+    },
     red: {
         backgroundColor: '#f5f5f5ab',
     },
-    header:{
-        backgroundColor: '#deb5b545',
-    }
+    // Define styles for regular cells
+    regularCell: {
+        padding: '10px',
+    },
 });
 
 export default CourseListRow;
