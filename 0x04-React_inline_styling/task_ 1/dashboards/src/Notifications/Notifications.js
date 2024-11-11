32e23eb76f511575474af54f95f6a2536783b148
modibo keita <93909081keita@gmail.com>
2024-11-11 18:50:12 +0200

    task_1

---
+++ 0x04-React_inline_styling/task_ 1/dashboards/src/Notifications/Notifications.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Notifications.css';
+
+class Notifications extends React.Component {
+  // Implement shouldComponentUpdate to optimize performance
+  shouldComponentUpdate(nextProps) {
+    // Only re-render if the length of the listNotifications has increased
+    return (
+      nextProps.listNotifications &&
+      nextProps.listNotifications.length > (this.props.listNotifications?.length || 0)
+    );
+  }
+
+  render() {
+    const { listNotifications = [] } = this.props; // Default to empty array if undefined
+
+    return (
+      <div className="Notifications">
+        <h1>Your Notifications</h1>
+        <ul>
+          {listNotifications.map((notification, index) => (
+            <li key={index}>{notification}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Notifications;
