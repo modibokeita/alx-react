4b6556699242dd99f1c193babed50192f7725701
modibo keita <93909081keita@gmail.com>
2024-11-11 18:57:59 +0200

    inline styles in Header component

--- 0x04-React_inline_styling/task_ 1/dashboards/src/Header/Header.js
+++ 0x04-React_inline_styling/task_ 1/dashboards/src/Header/Header.js
@@ -1,14 +1,33 @@
-import logo from "../logo-holberton.jpg"
+// src/Header/Header.js
 import React from 'react';
-import './Header.css';
+import { StyleSheet, css } from 'aphrodite';
+import logo from '../logo-holberton.jpg';
 
 function Header() {
   return (
-    <div className="App-header">
-    <img src={logo} alt="holberton logo" />
-    <h1>School dashboard</h1>
+    <div className={css(styles.header)}>
+      <img src={logo} alt="holberton logo" className={css(styles.logo)} />
+      <h1 className={css(styles.title)}>School dashboard</h1>
     </div>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    display: 'flex',
+    width: '100%',
+    fontSize: 'calc(10px + 2vmin)',
+    borderBottom: '5px solid red',
+    alignItems: 'center', // Aligns items vertically within the header
+  },
+  logo: {
+    marginRight: '15px',
+  },
+  title: {
+    padding: '100px',
+    color: 'red',
+    margin: 0,
+  },
+});
 
 export default Header;
