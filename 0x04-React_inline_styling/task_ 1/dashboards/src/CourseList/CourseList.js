39c500e39af1e67445b87c6394816c5a2ab20173
modibo keita <93909081keita@gmail.com>
2024-11-11 18:53:03 +0200

    inline styles in coureList

--- 0x04-React_inline_styling/task_ 1/dashboards/src/CourseList/CourseList.js
+++ 0x04-React_inline_styling/task_ 1/dashboards/src/CourseList/CourseList.js
@@ -1,11 +1,11 @@
 // src/CourseList/CourseList.js
 import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
 import CourseListRow from './CourseListRow';
-
 
 const CourseList = () => {
     return (
-        <table id="CourseList">
+        <table className={css(styles.courseList)}>
             <thead>
                 <CourseListRow isHeader textFirstCell="Available courses" />
                 <CourseListRow isHeader textFirstCell="Course name" textSecondCell="Credit" />
@@ -18,5 +18,22 @@ const CourseList = () => {
         </table>
     );
 };
+
+const styles = StyleSheet.create({
+    courseList: {
+        width: '100%',
+        borderCollapse: 'collapse',
+        margin: '20px 0',
+    },
+    th: {
+        backgroundColor: '#deb5b545',
+        padding: '10px',
+        textAlign: 'left',
+    },
+    td: {
+        padding: '10px',
+        border: '1px solid #ddd',
+    },
+});
 
 export default CourseList;
