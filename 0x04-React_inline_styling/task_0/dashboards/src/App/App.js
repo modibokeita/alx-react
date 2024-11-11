37d59039ed6ef92c8d6c3ab86bd3abacbba8ea07
modibo keita <93909081keita@gmail.com>
2024-11-11 17:52:43 +0200

    fix Notification component

--- 0x04-React_inline_styling/task_0/dashboards/src/App/App.js
+++ 0x04-React_inline_styling/task_0/dashboards/src/App/App.js
@@ -1,7 +1,7 @@
 // src/App/App.js
 import React from 'react';
 import './App.css';
-import {Notifications} from '../Notifications/Notifications';
+import Notifications from '../Notifications/Notifications';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Login from '../Login/Login';
--- 0x04-React_inline_styling/task_0/dashboards/src/Notifications/Notifications.js
+++ 0x04-React_inline_styling/task_0/dashboards/src/Notifications/Notifications.js
@@ -5,19 +5,20 @@ class Notifications extends React.Component {
   // Implement shouldComponentUpdate to optimize performance
   shouldComponentUpdate(nextProps) {
     // Only re-render if the length of the listNotifications has increased
-    if (nextProps.listNotifications.length > this.props.listNotifications.length) {
-      return true; // Allow re-render
-    }
-    return false; // Prevent re-render
+    return (
+      nextProps.listNotifications &&
+      nextProps.listNotifications.length > (this.props.listNotifications?.length || 0)
+    );
   }
 
   render() {
+    const { listNotifications = [] } = this.props; // Default to empty array if undefined
+
     return (
       <div className="Notifications">
         <h1>Note</h1>
-        {/* You can map over the notifications here */}
         <ul>
-          {this.props.listNotifications.map((notification, index) => (
+          {listNotifications.map((notification, index) => (
             <li key={index}>{notification}</li>
           ))}
         </ul>
