85245b4c68e54514e3487620af85c013f0ab79c4
modibo keita <93909081keita@gmail.com>
2024-11-18 15:34:39 +0200

    Login component

--- 0x05-react_state/task_1/dashboards/src/Login/Login.js
+++ 0x05-react_state/task_1/dashboards/src/Login/Login.js
@@ -1,19 +1,88 @@
-// src/Login/Login.js
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, css } from 'aphrodite';
 
-function Login() {
-  return (
-    <div className={css(styles.body)}>
-      <h1 className={css(styles.title)}>Log in to continue</h1>
-      <p className={css(styles.subtitle)}>Login to access the full dashboard</p>
-      <label htmlFor="email" className={css(styles.label)}>Email</label>
-      <input type="email" name="email" id="email" className={css(styles.input)} />
-      <label htmlFor="password" className={css(styles.label)}>Password</label>
-      <input type="password" name="password" id="password" className={css(styles.input)} />
-      <button className={css(styles.button)}>OK</button>
-    </div>
-  );
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+      email: '',
+      password: '',
+      enableSubmit: false,
+    };
+
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+    this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
+  }
+
+  handleChangeEmail(event) {
+    const email = event.target.value;
+    this.setState(
+      { email },
+      this.toggleSubmitButton // Callback to check if submit should be enabled
+    );
+  }
+
+  handleChangePassword(event) {
+    const password = event.target.value;
+    this.setState(
+      { password },
+      this.toggleSubmitButton // Callback to check if submit should be enabled
+    );
+  }
+
+  toggleSubmitButton() {
+    const { email, password } = this.state;
+    this.setState({ enableSubmit: email !== '' && password !== '' });
+  }
+
+  handleLoginSubmit(event) {
+    event.preventDefault();
+    this.setState({ isLoggedIn: true });
+  }
+
+  render() {
+    const { email, password, enableSubmit, isLoggedIn } = this.state;
+
+    return (
+      <div className={css(styles.body)}>
+        <h1 className={css(styles.title)}>Log in to continue</h1>
+        <p className={css(styles.subtitle)}>Login to access the full dashboard</p>
+        <form onSubmit={this.handleLoginSubmit}>
+          <label htmlFor="email" className={css(styles.label)}>Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={this.handleChangeEmail}
+            className={css(styles.input)}
+            required
+          />
+          <label htmlFor="password" className={css(styles.label)}>Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={this.handleChangePassword}
+            className={css(styles.input)}
+            required
+          />
+          <input
+            type="submit"
+            value="OK"
+            className={css(styles.button)}
+            disabled={!enableSubmit}
+          />
+        </form>
+        {isLoggedIn && (
+          <p className={css(styles.successMessage)}>You are logged in!</p>
+        )}
+      </div>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -37,7 +106,7 @@ const styles = StyleSheet.create({
   },
   input: {
     display: 'block',
-    width: '10%',
+    width: '30%',
     padding: '8px',
     marginBottom: '15px',
     borderRadius: '4px',
@@ -51,6 +120,11 @@ const styles = StyleSheet.create({
     border: 'none',
     borderRadius: '4px',
     cursor: 'pointer',
+  },
+  successMessage: {
+    marginTop: '20px',
+    color: 'green',
+    fontWeight: 'bold',
   },
 });
 
