9f3e18d015a863793ff6f72c9dbe40eee2a59596
modibo keita <93909081keita@gmail.com>
2024-11-18 15:27:47 +0200

    Notifications component

--- 0x05-react_state/task_0/dashboards/src/Notifications/Notifications.js
+++ 0x05-react_state/task_0/dashboards/src/Notifications/Notifications.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Notifications.css';
 
 class Notifications extends React.Component {
@@ -12,19 +13,69 @@ class Notifications extends React.Component {
   }
 
   render() {
-    const { listNotifications = [] } = this.props; // Default to empty array if undefined
+    const {
+      listNotifications = [],
+      displayDrawer,
+      handleDisplayDrawer,
+      handleHideDrawer,
+    } = this.props;
 
     return (
-      <div className="Notifications">
-        <h1>Your Notifications</h1>
-        <ul>
-          {listNotifications.map((notification, index) => (
-            <li key={index}>{notification}</li>
-          ))}
-        </ul>
-      </div>
+      <>
+        {/* Notification drawer toggle */}
+        <div
+          className="menuItem"
+          onClick={handleDisplayDrawer}
+          role="button"
+          tabIndex={0}
+          onKeyPress={handleDisplayDrawer}
+        >
+          <p>Your notifications</p>
+        </div>
+
+        {displayDrawer && (
+          <div className="Notifications">
+            <button
+              style={{
+                position: 'absolute',
+                right: '10px',
+                top: '10px',
+                background: 'none',
+                border: 'none',
+                cursor: 'pointer',
+              }}
+              aria-label="Close"
+              onClick={handleHideDrawer}
+            >
+              &times;
+            </button>
+            <h1>Your Notifications</h1>
+            <ul>
+              {listNotifications.map((notification, index) => (
+                <li key={index}>{notification}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </>
     );
   }
 }
+
+// Define propTypes for Notifications
+Notifications.propTypes = {
+  listNotifications: PropTypes.arrayOf(PropTypes.string),
+  displayDrawer: PropTypes.bool,
+  handleDisplayDrawer: PropTypes.func,
+  handleHideDrawer: PropTypes.func,
+};
+
+// Define defaultProps for Notifications
+Notifications.defaultProps = {
+  listNotifications: [],
+  displayDrawer: false,
+  handleDisplayDrawer: () => {},
+  handleHideDrawer: () => {},
+};
 
 export default Notifications;
--- 0x05-react_state/task_0/dashboards/src/Notifications/Notifications.test.js
+++ 0x05-react_state/task_0/dashboards/src/Notifications/Notifications.test.js
@@ -4,43 +4,39 @@ import Notifications from './Notifications';
 
 describe('Notifications component', () => {
   it('should not re-render when updating with the same list', () => {
-    // Initial list of notifications
     const listNotifications = ['Notification 1', 'Notification 2'];
-
-    // Spy on the render method to track rerenders
     const spy = jest.spyOn(Notifications.prototype, 'render');
-
-    // Shallow render the component
     const wrapper = shallow(<Notifications listNotifications={listNotifications} />);
-
-    // Set the same props again (same list)
     wrapper.setProps({ listNotifications });
-
-    // Check that the render method has not been called again
     expect(spy).toHaveBeenCalledTimes(1);
-
-    // Restore the spy
     spy.mockRestore();
   });
 
   it('should re-render when updating with a longer list', () => {
-    // Initial list of notifications
     const listNotifications = ['Notification 1', 'Notification 2'];
-
-    // Spy on the render method to track rerenders
     const spy = jest.spyOn(Notifications.prototype, 'render');
-
-    // Shallow render the component
     const wrapper = shallow(<Notifications listNotifications={listNotifications} />);
-
-    // Set new props with a longer list
     const newListNotifications = ['Notification 1', 'Notification 2', 'Notification 3'];
     wrapper.setProps({ listNotifications: newListNotifications });
-
-    // Check that the render method has been called again because the list is longer
     expect(spy).toHaveBeenCalledTimes(2);
-
-    // Restore the spy
     spy.mockRestore();
+  });
+
+  it('should call handleDisplayDrawer when clicking on the menu item', () => {
+    const handleDisplayDrawerMock = jest.fn();
+    const wrapper = shallow(
+      <Notifications handleDisplayDrawer={handleDisplayDrawerMock} />
+    );
+    wrapper.find('.menuItem').simulate('click');
+    expect(handleDisplayDrawerMock).toHaveBeenCalled();
+  });
+
+  it('should call handleHideDrawer when clicking on the close button', () => {
+    const handleHideDrawerMock = jest.fn();
+    const wrapper = shallow(
+      <Notifications displayDrawer={true} handleHideDrawer={handleHideDrawerMock} />
+    );
+    wrapper.find('button[aria-label="Close"]').simulate('click');
+    expect(handleHideDrawerMock).toHaveBeenCalled();
   });
 });
