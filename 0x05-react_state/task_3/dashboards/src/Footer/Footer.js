25263abd56333230e4b7a5b78e39f5a23e1d3b4b
modibo keita <93909081keita@gmail.com>
2024-11-18 16:04:44 +0200

    footer component

--- 0x05-react_state/task_3/dashboards/src/Footer/Footer.js
+++ 0x05-react_state/task_3/dashboards/src/Footer/Footer.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Footer.css';
 import { getFooterCopy, getFullYear } from "../utils/utils";
-
+import { AppContext } from '../App/AppContext'; // Import the context
 
 function Footer() {
+  // Use useContext to access the user context
+  const { user } = useContext(AppContext);
+
   return (
     <div className="App-footer">
-        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
-   </div>
+      <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+
+      {/* Conditionally render the Contact Us link when user is logged in */}
+      {user.isLoggedIn && (
+        <p>
+          <a href="/contact-us">Contact us</a>
+        </p>
+      )}
+    </div>
   );
 }
 
