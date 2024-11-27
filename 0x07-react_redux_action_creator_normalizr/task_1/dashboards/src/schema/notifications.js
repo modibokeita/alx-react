58c468bc083faa565550bb3961b6de8c1f99d91b
modibo keita <93909081keita@gmail.com>
2024-11-27 18:53:51 +0200

    testint for notification

--- 0x07-react_redux_action_creator_normalizr/task_1/dashboards/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_1/dashboards/src/schema/notifications.js
@@ -1,7 +1,22 @@
-import * as notificationsData from '../../../notifications.json';
+// src/schema/notifications.js
+import { normalize, schema } from 'normalizr';
+import * as notificationsData from '../../notifications.json';
 
-export function getAllNotificationsByUser(userId) {
-  return notificationsData.default.filter(
-    (notification) => notification.author.id === userId
-  ).map((notification) => notification.context);
-}
+// Define the user entity
+const user = new schema.Entity('users');
+
+// Define the message entity
+const message = new schema.Entity('messages', {}, { idAttribute: 'guid' });
+
+// Define the notification entity
+const notification = new schema.Entity('notifications', {
+  author: user,
+  context: message,
+});
+
+// Normalize the notifications data
+export const normalizedData = normalize(notificationsData.default, [notification]);
+
+// Export entities for testing
+export { user, message, notification };
+
