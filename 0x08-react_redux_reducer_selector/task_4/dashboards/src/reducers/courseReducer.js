0d9bd466c336b0307e1be232af8f41ebcfe5fded
modibo keita <93909081keita@gmail.com>
2024-12-02 19:59:34 +0200

    course UI

---
+++ 0x08-react_redux_reducer_selector/task_4/dashboards/src/reducers/courseReducer.js
@@ -0,0 +1,28 @@
+import { fromJS, Map } from 'immutable';
+import { FETCH_COURSE_SUCCESS, SELECT_COURSE, UNSELECT_COURSE } from '../actions/courseActionTypes';
+import { coursesNormalizer } from '../schema/courses';
+
+const initialState = Map({
+  courses: Map(),
+});
+
+const courseReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_COURSE_SUCCESS:
+      const normalizedData = coursesNormalizer(action.data);
+      return state.merge({
+        courses: fromJS(normalizedData.entities.courses),
+      });
+
+    case SELECT_COURSE:
+      return state.setIn(['courses', action.index.toString(), 'isSelected'], true);
+
+    case UNSELECT_COURSE:
+      return state.setIn(['courses', action.index.toString(), 'isSelected'], false);
+
+    default:
+      return state;
+  }
+};
+
+export default courseReducer;
