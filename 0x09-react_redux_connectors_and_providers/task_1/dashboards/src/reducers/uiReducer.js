15752a213a85d43f3a0cde01cd757f75f833d0c1
modibo keita <93909081keita@gmail.com>
2024-12-06 22:06:09 +0200

    task_1

---
+++ 0x09-react_redux_connectors_and_providers/task_1/dashboards/src/reducers/uiReducer.js
@@ -0,0 +1,38 @@
+import { Map } from 'immutable';
+import {
+  DISPLAY_NOTIFICATION_DRAWER,
+  HIDE_NOTIFICATION_DRAWER,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+  LOGOUT,
+} from '../actions/uiActionTypes';
+
+const initialState = Map({
+  isNotificationDrawerVisible: false,
+  isUserLoggedIn: false,
+  user: {},
+});
+
+/**
+ * Reducer for the UI state using Immutable.js
+ * @param {Map} state - Current state as an Immutable Map.
+ * @param {Object} action - Action dispatched.
+ * @returns {Map} - New state as an Immutable Map.
+ */
+const uiReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case DISPLAY_NOTIFICATION_DRAWER:
+      return state.set('isNotificationDrawerVisible', true);
+    case HIDE_NOTIFICATION_DRAWER:
+      return state.set('isNotificationDrawerVisible', false);
+    case LOGIN_SUCCESS:
+      return state.set('isUserLoggedIn', true);
+    case LOGIN_FAILURE:
+    case LOGOUT:
+      return state.set('isUserLoggedIn', false);
+    default:
+      return state;
+  }
+};
+
+export default uiReducer;
