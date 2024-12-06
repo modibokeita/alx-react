0e47c6959f0ac73b5a703e4d9646ab05bb140d02
modibo keita <93909081keita@gmail.com>
2024-12-06 22:20:18 +0200

    provider

--- 0x09-react_redux_connectors_and_providers/task_2/dashboards/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboards/src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk'; // Import redux-thunk
+import uiReducer from './reducers/uiReducer';
 import App from './App/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
+// Create the store with thunk middleware
+const store = createStore(uiReducer, applyMiddleware(thunk));
 
-  </React.StrictMode>
+// Wrap the App component with the Provider and pass the store
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
+
 
 
 
